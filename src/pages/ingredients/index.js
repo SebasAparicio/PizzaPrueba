1e9cddb1b89ace82de52c41b24b915fda190e123
SebasApr <sebasaparicio20@gmail.com>
2021-03-12 09:46:36 +0100

    css for ingredient

--- src/pages/ingredients/index.js
+++ src/pages/ingredients/index.js
@@ -3,12 +3,12 @@ import { Page } from "../../components";
 import "../../styles";
 import { IngredientList } from "./components/ingredient/ingredient-list";
 import { IngredientAdd } from "./components/ingredient/ingredient-add";
-
+import css from './index.module.css'
 function Ingredients({ ingredients }) {
   return (
     <Page>
-      <main>
-        <h3>Todos los ingredientes</h3>
+      <main className = {css.main}>
+        <h3 className = {css.h3}>Todos los ingredientes</h3>
         <IngredientList ingredients={ingredients} />
         <IngredientAdd />
       </main>
