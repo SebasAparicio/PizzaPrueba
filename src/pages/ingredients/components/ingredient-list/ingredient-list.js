5deaa3aa8a5d9ff469f61bc7e110e97be01a7301
SebasApr <sebasaparicio20@gmail.com>
2021-03-12 13:11:34 +0100

    modified ingredient-list

--- src/pages/ingredients/components/ingredient-list/ingredient-list.js
+++ src/pages/ingredients/components/ingredient-list/ingredient-list.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Ingredient } from "./ingredient";
-import css from './ingredient.module.css';
+import css from "./ingredient.module.css";
 
 export function IngredientList({ ingredients }) {
-  return ( //<section> y h2
-      <ul className = {css.ul}>
+  return (
+    //<section> y h2
+    <section>
+      <h2>Todos los ingredientes</h2>
+      <ul className={css.ul}>
         {ingredients.map((ingredient) => {
           return (
             <li key={ingredient.id}>
-              <Ingredient ingredient = {ingredient}/>
+              <Ingredient ingredient={ingredient} />
             </li>
           );
         })}
       </ul>
-
+    </section>
   );
 }
--- src/pages/ingredients/components/ingredient-list/ingredient.js
+++ src/pages/ingredients/components/ingredient-list/ingredient.js
@@ -3,10 +3,10 @@ import css from './ingredient.module.css';
 
 export function Ingredient({ingredient}){
     return(
-        <article className = {css.article}>
+        
         <h3>
             {ingredient.name} ({ingredient.price}€)
         </h3>
-      </article>
+      
     )
 }
