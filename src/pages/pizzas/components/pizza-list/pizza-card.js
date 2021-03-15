8f3f98bdc936e06bf8bc9ae7c4bef242668df2c3
eva <evinchy@hotmail.com>
2021-03-15 23:33:36 +0100

    Changes CSS pizza-list, pizza-card

--- src/pages/pizzas/components/pizza-list/pizza-card.js
+++ src/pages/pizzas/components/pizza-list/pizza-card.js
@@ -5,9 +5,9 @@ import css from "./pizza-card.module.css"
 export function PizzaCard({pizza}) {
   return (
     <article className={css.card}>
-      <h3>{pizza.image}</h3>
-      <h3>{pizza.name}</h3>
-      <h4>{pizza.price}€</h4>
+      <h1>Image</h1>
+      <h3 className={css.h3}>{pizza.name}</h3>
+      <h4 className={css.h4}>{pizza.price}€</h4>
       <h3 className={css.buttonContainer}>
         <Button className={css.buttonCarrito} label="AÑADIR AL CARRITO"/><Button className={css.buttonVer} label="VER MAS"/>
       </h3>
--- src/pages/pizzas/components/pizza-list/pizza-list.js
+++ src/pages/pizzas/components/pizza-list/pizza-list.js
@@ -4,11 +4,7 @@ import css from "./pizza-list.module.css"
 
 export function PizzaList({pizzas}) {
   return (
-<<<<<<< HEAD
       <ul className={css.list}>
-=======
-      <ul>
->>>>>>> 12e5f819e810f45637c49dad5b9e6e04dad44687
         {pizzas.map((pizza)=>{
         return(
           <li key={pizza.id}>
