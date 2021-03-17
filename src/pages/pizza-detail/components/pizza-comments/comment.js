afbd567970b7075446e14fceb38c2ddc11cf5d0f
PeCorona <corona777sga@hotmail.es>
2021-03-17 12:52:08 +0100

    TimeService done

--- src/pages/pizza-detail/components/pizza-comments/comment.js
+++ src/pages/pizza-detail/components/pizza-comments/comment.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {TimeService} from '../../../../services/time-service';
 
 export function Comment ({comment}){
     const {date, text, rating} = comment;
     return(
         <div>
-            <div>{date}</div>
+            <div>{TimeService.shortDate(date)}</div>
             <div>{rating}</div>
             <div>{text}</div>
         </div>
