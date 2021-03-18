feacd554310ca29d0950752ba06307f12e4573c2
SebasApr <sebasaparicio20@gmail.com>
2021-03-18 10:06:50 +0100

    Added css in comment

--- src/pages/pizza-detail/components/pizza-comments/comment.js
+++ src/pages/pizza-detail/components/pizza-comments/comment.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {TimeService} from '../../../../services/time-service';
+import css from './comment.module.css';
 
 export function Comment ({comment}){
     const {date, text, rating} = comment;
     return(
         <div>
-            <div>{TimeService.shortDate(date)}</div>
-            <div>Puntuacion {rating}</div>
+            <div className={css.date}>{TimeService.shortDate(date)}</div>
+            <div className={css.rating}>Puntuacion {rating}</div>
             <div>{text}</div>
         </div>
     )
--- src/pages/pizza-detail/components/pizza-comments/commentList.js
+++ src/pages/pizza-detail/components/pizza-comments/commentList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Comment} from './comment.js';
-
+import css from './commentList.module.css';
 export function CommentList({ comments }) {
     return (
       <section>
         <ul>
           {comments.map((comment) => {
             return (
-              <li key={comment.id}>
+              <li className={css.comment} key={comment.id}>
                  <Comment comment = {comment}/>
               </li>
             );
