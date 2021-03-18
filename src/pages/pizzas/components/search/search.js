1eca71ef208c9856a5513492a2f910ecb9e0af80
SebasApr <sebasaparicio20@gmail.com>
2021-03-18 10:23:10 +0100

    Search modified

--- src/pages/pizzas/components/search/search.js
+++ src/pages/pizzas/components/search/search.js
@@ -5,7 +5,6 @@ export function Search(){
     return(
         <Form className={css.form}>
             <Input placeholder="Busca tu pizza favorita" className={css.search}></Input>
-            <Input placeholder="Filtrar" className={css.filterAndArrange}></Input>
             <Input placeholder="Ordenar" className={css.filterAndArrange}></Input>
         </Form>
     )
